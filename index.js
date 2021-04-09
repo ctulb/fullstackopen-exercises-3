09fbb6ed507dbaabfcdfa7ede893c6ad07603a72
Leigh Butler <24991566+ctulb@users.noreply.github.com>
2021-04-09 15:41:13 +0100

    complete exercise 3.7

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
 
@@ -25,6 +26,7 @@ var persons = [
   },
 ];
 
+app.use(morgan("tiny"));
 app.use(express.json());
 
 const PORT = 3001;
