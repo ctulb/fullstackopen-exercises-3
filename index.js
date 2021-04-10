8b67251f126704418a95d66a0bf73a19f88e4996
Leigh Butler <24991566+ctulb@users.noreply.github.com>
2021-04-10 17:53:38 +0100

    complete exercise 3.14

--- index.js
+++ index.js
@@ -1,96 +1,98 @@
-const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
 
-var persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    phoneNumber: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    phoneNumber: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    phoneNumber: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendick",
-    phoneNumber: "39-23-6423122",
-  },
-];
+const Person = require('./models/person');
 
 app.use(cors());
-app.use(express.static("build"));
-morgan.token("reqBody", (req, res) => {
+app.use(express.static('build'));
+morgan.token('reqBody', (req, res) => {
   return JSON.stringify(req.body);
 });
 app.use(
   morgan(
-    ":method :url :status :res[content-length] - :response-time ms :reqBody"
+    ':method :url :status :res[content-length] - :response-time ms :reqBody'
   )
 );
 app.use(express.json());
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 
-app.get("/api/persons", (req, res) => {
-  res.json(persons);
+app.get('/api/persons', (req, res) => {
+  Person.find({})
+    .then((documents) => {
+      res.json(documents);
+    })
+    .catch((reason) => {
+      res.status(500).json({ error: reason });
+    });
 });
 
-app.post("/api/persons", (req, res) => {
+app.post('/api/persons', (req, res) => {
   if (!req.body.name || !req.body.phoneNumber) {
     return res
       .status(400)
-      .json({ error: "request is missing required fields" });
+      .json({ error: 'request is missing required fields' });
   }
-  if (persons.find((person) => person.name === req.body.name)) {
-    return res.status(400).json({ error: "name already exists" });
-  }
-  id = Math.floor(Math.random() * 100000);
-  const newPerson = {
-    id,
+  Person.find({ name: req.body.name }).then((document) => {
+    if (Object.keys(document).length > 0) {
+      return res.status(400).json({ error: 'name already exists' });
+    }
+  });
+  const newPerson = new Person({
     name: req.body.name,
     phoneNumber: req.body.phoneNumber,
-  };
-  persons.push(newPerson);
-  res.status(201).json(newPerson);
+  });
+  newPerson
+    .save()
+    .then((document) => {
+      res.status(201).json(document);
+    })
+    .catch((reason) => {
+      res.status(500).json({ error: reason });
+    });
 });
 
-app.get("/api/persons/:id", (req, res) => {
-  const wantedPersonId = Number(req.params.id);
-  const foundPerson = persons.find((person) => person.id === wantedPersonId);
-  if (foundPerson) {
-    return res.json(foundPerson);
-  } else {
-    return res.sendStatus(404);
-  }
+app.get('/api/persons/:id', (req, res) => {
+  Person.findById(req.params.id)
+    .then((document) => {
+      if (document) {
+        return res.json(document);
+      } else {
+        return res.sendStatus(404);
+      }
+    })
+    .catch((reason) => {
+      res.status(500).json({ error: reason });
+    });
 });
 
-app.delete("/api/persons/:id", (req, res) => {
-  const wantedPersonId = Number(req.params.id);
-  const foundPerson = persons.find((person) => person.id === wantedPersonId);
-  if (foundPerson) {
-    persons = persons.filter((person) => person.id !== foundPerson.id);
-    return res.sendStatus(204);
-  } else {
-    return res.sendStatus(404);
-  }
+app.delete('/api/persons/:id', (req, res) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then((document) => {
+      if (document) {
+        res.sendStatus(204);
+      } else {
+        res.sendStatus(404);
+      }
+    })
+    .catch((reason) => {
+      res.status(500).json({ error: reason });
+    });
 });
 
-app.get("/info", (req, res) => {
-  const entries = persons.length;
+app.get('/info', (req, res) => {
   const datestamp = new Date();
-  res.send(
-    `<p>Phonebook has info for ${entries} people</p><p>${datestamp}</p>`
-  );
+  Person.countDocuments({}).then((err, count) => {
+    if (err) {
+      res.send('<p>Error retrieving phonebook data.</p>');
+    } else {
+      res.send(`<p>Phonebook has info for ${count} people</p><p>${datestamp}`);
+    }
+  });
 });
 
 app.listen(PORT, () => {
---
+++ models/person.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+if (!process.env.MONGODB_CONNECTION_STRING) {
+  console.error('MongoDB connection string could not be configured');
+  process.exit(1);
+}
+
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((result) => {
+    console.log('Connected to database');
+  })
+  .catch((reason) => {
+    console.error(`Could not connect to database: ${reason}`);
+    process.exit(1);
+  });
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  phoneNumber: String,
+});
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject._v;
+  },
+});
+
+module.exports = mongoose.model('Person', personSchema);
